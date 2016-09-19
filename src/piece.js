530497158335a1bfbf6819cf16f3207367729707
Ryan Cook <ryan@ryancook.software>
2016-09-18 21:24:57 -0400

    Created null piece

--- src/piece.js
+++ src/piece.js
@@ -1,5 +1,4 @@
-
-class Piece {
+export class Piece {
     /**
      * Create a piece.
      * @param {string} color - The color of the piece: w|b.
@@ -7,6 +6,14 @@ class Piece {
      */
     constructor(color) {
         this.color = color;
+    }
+
+    /**
+     * Get the type.
+     * @return {string} The type: p|n|b|r|q|k.
+     */
+    get type() {
+        return this._type;
     }
 
     /**
@@ -47,20 +54,103 @@ class Piece {
 
     /**
      * Determine if the pieces matches the provided color.
-     * @param {string} The color to match against: w|b.
+     * @param {string} color - The color to match against: w|b.
      * @return {boolean} True if the piece color matches the provided color, otherwise false.
      */
     isColor(color) {
         return (this._color === color);
     }
+
+    /**
+     * Determine if two pieces are logically equal.
+     * @param {Piece} piece - The piece to compare against.
+     * @return {boolean} True if the pieces have the same type and color, otherwise false.
+     */
+    equals(piece) {
+        return (this._color === piece._color && this._type === piece._type);
+    }
+
+    /**
+     * Factory method for creating a piece, when the type is unknown until runtime.
+     * @param {string} The piece type and the color. Examples: wp, bn
+     * @return {Piece} A piece of the appropriate type.
+     */
+    static create(pieceAndColor) {
+        let pieceType,
+            color;
+
+        if (typeof pieceAndColor === 'string' && pieceAndColor.length === 2) {
+            pieceType = pieceAndColor.substr(1, 1);
+            color = pieceAndColor.substr(0, 1);
+        }
+
+        switch (pieceType) {
+            case 'p':
+                return new Pawn(color);
+            case 'n':
+                return new Knight(color);
+            case 'b':
+                return new Bishop(color);
+            case 'r':
+                return new Rook(color);
+            case 'q':
+                return new Queen(color);
+            case 'k':
+                return new King(color);
+            default:
+                return new NullPiece();
+        }
+    }
 }
 
-export class Pawn extends Piece {}
-export class Knight extends Piece {}
-export class Bishop extends Piece {}
-export class Rook extends Piece {}
-export class Queen extends Piece {}
-export class King extends Piece {}
+export class Pawn extends Piece {
+    constructor(color) {
+        super(color);
+        this._type = 'p';
+    }
+}
+
+export class Knight extends Piece {
+    constructor(color) {
+        super(color);
+        this._type = 'n';
+    }
+}
+
+export class Bishop extends Piece {
+    constructor(color) {
+        super(color);
+        this._type = 'b';
+    }
+}
+
+export class Rook extends Piece {
+    constructor(color) {
+        super(color);
+        this._type = 'r';
+    }
+}
+
+export class Queen extends Piece {
+    constructor(color) {
+        super(color);
+        this._type = 'q';
+    }
+}
+
+export class King extends Piece {
+    constructor(color) {
+        super(color);
+        this._type = 'k';
+    }
+}
+
+export class NullPiece {
+    constructor() {
+        this.color = '';
+        this.type = '';
+    }
+}
 
 function validColor (c) {
     return (/^w|b$/.test(c));
--- src/piece.spec.js
+++ src/piece.spec.js
@@ -1,6 +1,6 @@
 /* eslint-env jasmine */
 
-import { Pawn, Knight, Bishop } from './piece';
+import { Piece, Pawn, Knight, Bishop, Rook, Queen, King, NullPiece } from './piece';
 
 describe('Piece', () => {
 
@@ -20,6 +20,45 @@ describe('Piece', () => {
         expect(() => { new Pawn(''); }).toThrow();
         expect(() => { new Knight('W'); }).toThrow();
         expect(() => { new Bishop('black'); }).toThrow();
+    });
+
+    it('has a type', () => {
+        expect(new Pawn('w').type).toBe('p');
+        expect(new Knight('w').type).toBe('n');
+        expect(new Bishop('w').type).toBe('b');
+        expect(new Rook('w').type).toBe('r');
+        expect(new Queen('w').type).toBe('q');
+        expect(new King('w').type).toBe('k');
+        expect(new NullPiece().type).toBe('');
+    });
+
+    it('can create a piece from a string', () => {
+        let pawn = Piece.create('wp'),
+            knight = Piece.create('bn'),
+            bishop = Piece.create('wb'),
+            rook = Piece.create('br'),
+            queen = Piece.create('wq'),
+            king = Piece.create('bk'),
+            nullPiece = Piece.create('');
+
+        expect(pawn instanceof Pawn).toBe(true);
+        expect(knight instanceof Knight).toBe(true);
+        expect(bishop instanceof Bishop).toBe(true);
+        expect(rook instanceof Rook).toBe(true);
+        expect(queen instanceof Queen).toBe(true);
+        expect(king instanceof King).toBe(true);
+        expect(nullPiece instanceof NullPiece).toBe(true);
+    });
+
+    it('can be compared to another piece for logical equality', () => {
+        let whiteQueen = new Queen('w'),
+            whiteRook = new Rook('w'),
+            blackQueen = new Queen('b'),
+            anotherWhiteQueen = new Queen('w');
+
+        expect(whiteQueen.equals(whiteRook)).toBe(false);
+        expect(whiteQueen.equals(blackQueen)).toBe(false);
+        expect(whiteQueen.equals(anotherWhiteQueen)).toBe(true);
     });
 
 });
